68cb8e54c2824832d73f171b691cc264dd4ddfa7
Diogo Matos <diogo.matos@gmail.com>
2019-09-17 12:12:14 +0100

    adding the server functionalities

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
@@ -8,28 +9,25 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-
-  heroes: Hero[]
-  selectedHero: Hero;
-
-  onSelect(hero: Hero): void {
-    console.log(hero);
-    this.selectedHero = hero;
-  }
+  heroes: Hero[];
 
   constructor(private heroService: HeroService) { }
-
-  getHeroes(): void {
-    console.log(" *** Get the list of heroes");
-    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
-  }
 
   ngOnInit() {
     this.getHeroes();
   }
 
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
 }
